54d43a62b58c1b89a54bda2358d6927d8bcf0e99
ashar-aala <ashar@aalasolutions.com>
2025-01-13 16:17:49 +0500

    added Kindesdk directory

---
+++ KindeSrc/environment/pages/(kinde)/(sign_in)/page.tsx
@@ -0,0 +1,34 @@
+import {
+  getKindeCSRF,
+  getKindeRequiredCSS,
+  getKindeRequiredJS,
+} from "@kinde/infrastructure";
+
+const Page = () => {
+  return `
+          <html lang="en">
+              <head>
+                  <meta charset="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta name="robots" content="noindex" />
+      <meta name="csrf-token" content="${getKindeCSRF}" />
+      <title>Hello world</title>
+      ${getKindeRequiredCSS()}
+      ${getKindeRequiredJS()}
+    </head>
+    <body>
+      <dev>
+        <header>
+          <h1>Componey Name</h1>
+        </header>
+        <main>
+          <h6>How are you</h6>
+        </main>
+      </dev>
+    </body>
+  </html>
+  
+          `;
+};
+
+export default Page;
