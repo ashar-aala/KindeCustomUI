5bb2618d465ef1e6ac03d526d53cf89bc72c427c
ashar-aala <ashar@aalasolutions.com>
2025-01-14 10:11:09 +0500

    change index to default

---
+++ kindeSrc/environment/pages/(kinde)/(default)/page.tsx
@@ -0,0 +1,36 @@
+import {
+  getKindeCSRF,
+  getKindeRequiredCSS,
+  getKindeRequiredJS,
+  getKindeWidget,
+} from "@kinde/infrastructure";
+
+const Page = () => {
+  return `
+        <html lang="en">
+            <head>
+                <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="robots" content="noindex" />
+    <meta name="csrf-token" content="${getKindeCSRF}" />
+    <title>Hello world</title>
+    ${getKindeRequiredCSS()}
+    ${getKindeRequiredJS()}
+  </head>
+  <body>
+    <dev>
+      <header>
+        <h1>Company Name</h1>
+      </header>
+      <main>
+        <h6>How are you</h6>
+        ${getKindeWidget()}
+      </main>
+    </dev>
+  </body>
+</html>
+
+        `;
+};
+
+export default Page;
